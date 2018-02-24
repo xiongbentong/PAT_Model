c991ca1b910dc583ce259d9f7eb6af9fd92e2b1a
白凌南 <32591542+bailingnan@users.noreply.github.com>
2018-02-24 17:58:35 +0800

    Create 二叉树的静态实现.cpp

---
+++ 9.DS-2/二叉树的静态实现.cpp
@@ -0,0 +1,68 @@
+struct node {
+	typename data;
+	int lchild;
+	int rchild;
+}Node[maxn];
+int index = 0;
+int newNode(int v) {
+	Node[index].data = v;
+	Node[index].lchild = -1;
+	Node[index].rchild = -1;
+	return index++;
+}
+void search(int root, int x, int newdata) {
+	if (root == -1) {
+		return;
+	}
+	if (Node[root].data == x) {
+		Node[root].data = newdata;
+	}
+	search(Node[root].lchild, x, newdata);
+	search(Node[root].rchild, x, newdata);
+}
+void insert(int &root, int x) {
+	if (root == -1) {
+		root = newNode(x);
+		return;
+	}
+	if (由二叉树的性质x应该插在左子树) {
+		insert(Node[root].lchild, x);
+	}
+	else {
+		insert(Node[root].rchild, x);
+	}
+}
+int Create(int data[], int n) {
+	int root = -1;
+	for (int i = 0; i < n; i++) {
+		insert(root, data[i]);
+	}
+	return root;
+}
+//先序遍历
+void preorder(int root) {
+	if (root == -1) {
+		return;
+	}
+	printf("%d\n", root->data);
+	preorder(Node[root].lchild);
+	preorder(Node[root].rchild);
+}
+//中序遍历
+void inorder(int root) {
+	if (root == -1) {
+		return;
+	}
+	inorder(Node[root].lchild);
+	printf("%d\n", root->data);
+	inorder(Node[root].rchild);
+}
+//后序遍历
+void postorder(int root) {
+	if (root == -1) {
+		return;
+	}
+	postorder(Node[root].lchild);
+	postorder(Node[root].rchild);
+	printf("%d\n", root->data);
+}
