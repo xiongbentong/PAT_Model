fabf2d73bda462fe4415c271ec259aba8e3d7630
白凌南 <32591542+bailingnan@users.noreply.github.com>
2018-02-27 01:50:12 +0800

    Update 日期处理.cpp

--- 3.base/日期处理.cpp
+++ 3.base/日期处理.cpp
@@ -29,3 +29,65 @@ int main() {
 	printf("%d", ans);
 	return 0;
 }
+#include<cstdio>
+struct Time {
+	int y;
+	int m;
+	int d;
+}t1,t2;
+bool Is366(int year){
+	return (year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0);
+}
+int month(int year, int month){
+	int Is31[7] = { 1,3,5,7,8,10,12 };
+	for (int i = 0; i < 7; i++){
+		if (Is31[i] == month)
+			return 31;
+	}
+	if (month == 2){
+		if (Is366(year))
+			return 29;
+		else return 28;
+	}
+	else return 30;
+}
+void swap(struct Time *a, struct Time *b) {
+	struct Time temp;
+	temp=*a;
+	*a = *b;
+	*b = temp;
+}
+int main(){	
+	int val;
+	while (scanf("%4d%2d%2d %4d%2d%2d", &t1.y, &t1.m, &t1.d, &t2.y, &t2.m, &t2.d) != EOF) {
+		if (t1.y > t2.y) swap(&t1, &t2);
+		else if(t1.y==t2.y){
+			if (t1.m > t2.m) swap(&t1, &t2);
+			else if(t1.m==t2.m)
+				if(t1.d>t2.d) swap(&t1, &t2);
+		}
+		if (t1.y == t2.y) {
+			if (t1.m == t2.m)
+				val = t2.d - t1.d + 1;
+			else {
+				val = month(t1.y, t1.m) - t1.d + 1 + t2.d;
+				for (int i = t1.m + 1; i <= t2.m - 1; i++)
+					val += month(t1.y, i);
+			}
+		}
+		else {
+			val = month(t1.y, t1.m) - t1.d + 1;
+			for (int i = t1.m + 1; i <= 12; i++)
+				val += month(t1.y, i);
+			for (int i = t1.y + 1; i < t2.y; i++) {
+				if (Is366(i)) val += 366;
+				else val += 365;
+			}
+			for (int i = 1; i < t2.m; i++)
+				val += month(t2.y, i);
+			val += t2.d;
+		}
+		printf("%d\n", val);
+	}
+	return 0;
+}
