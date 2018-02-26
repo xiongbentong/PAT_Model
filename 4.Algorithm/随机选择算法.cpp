fe04f4ef26782e2916ae54ad47ac3ba946a6b9ec
白凌南 <32591542+bailingnan@users.noreply.github.com>
2018-02-27 00:37:19 +0800

    Create 随机选择算法.cpp

---
+++ 4.Algorithm/随机选择算法.cpp
@@ -0,0 +1,13 @@
+//随机选择算法，从A[left,right]中返回第K大的数
+int randSelect(int A[], int left, int right, int K) {
+	if (left == right) return A[left];//边界
+	int p = randPartition(A, left, right);
+	int M = p - left + 1;
+	if (K == M) return A[p];
+	if (K < M) {
+		return randSelect(A, left, p - 1, K);
+	}
+	else {
+		return randSelect(A, p + 1, right, K - M);
+	}
+}
